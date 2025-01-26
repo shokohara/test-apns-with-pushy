5b4a6c13255f115665dd91598279794dfd4bc76a
KOHARA Sho <shokohara@users.noreply.github.com>
2025-01-26 19:08:54 +0900

    cat cacerts

--- src/Main.scala
+++ src/Main.scala
@@ -1,12 +1,23 @@
 //> using dep com.eatthepath:pushy:0.15.0
+//> using dep org.slf4j:slf4j-simple:2.0.16
 
 import io.netty.bootstrap.Bootstrap
 import io.netty.channel.{ChannelHandlerAdapter, ChannelHandlerContext, ChannelInitializer}
 import io.netty.channel.nio.NioEventLoopGroup
 import io.netty.channel.socket.SocketChannel
 import io.netty.channel.socket.nio.NioSocketChannel
+import io.netty.handler.codec.http2.{DefaultHttp2Connection, HttpToHttp2ConnectionHandlerBuilder}
 
 @main def hello() =
+  System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "DEBUG")
+
+  new ChannelInitializer[SocketChannel]:
+    override def initChannel(ch: SocketChannel): Unit =
+      val c = new DefaultHttp2Connection(false)
+      val handler = new HttpToHttp2ConnectionHandlerBuilder()
+      ch.pipeline().addLast()
+
+def case1 =
   val b = new Bootstrap()
   b.group(new NioEventLoopGroup()).channel(classOf[NioSocketChannel]).handler(
     new ChannelInitializer[SocketChannel]:
